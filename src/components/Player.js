c57d8573473d0a0f59ffff4913d4c44d362eb1ac
Никита Ковров <tochly@yandex.ru>
2020-11-11 21:13:35 +0300

    update: basic player functionality

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,7 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import throttle from "../utils/throttle";
 import playlist from "../constants/playlist";
+import PlayerTimebar from "./PlayerTimebar";
 import PlaylistItem from "./PlaylistItem";
 
 const Player = () => {
@@ -13,15 +14,42 @@ const Player = () => {
   // isPlaying
   const [isPlaying, setIsPlaying] = useState(false);
 
+  const myAudio = useRef(null);
+
   const onTimeUpdate = throttle((e) => {
-    console.log(e.target.duration);
-    setCurrentTIme(e.target.duration - e.target.currentTime);
+    setCurrentTIme(e.target.currentTime);
   }, 1000);
+
+  const onPlay = (e) => {
+    setDuration(e.target.duration);
+  }
+
+  const onPause = (e) => {
+    console.log('pause')
+  }
+
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const second = Math.floor(time % 60);
+    const seconds = second < 9 ? `0${second}` : second
+
+    return `${minutes}:${seconds}`
+  }
 
   return (
     <div className="header__player-block player">
       <div className="player__controller">
-        <button className="player__button player__button_play"></button>
+        <button className={`player__button player__button_${isPlaying ? 'stop' : 'play'}`} 
+          onClick={_ => {
+            if (isPlaying) {
+              myAudio.current.pause();
+              setIsPlaying(true);
+            } else {
+              myAudio.current.play();
+              setIsPlaying(true);
+            }
+          }}
+        ></button>
         <div className="player__song-container">
           <div className="player__current-song">
             <p className="player__song-title">
@@ -30,18 +58,30 @@ const Player = () => {
             </p>
           </div>
           <p className="player__song-duration">
-            {Math.floor(currentTime / 60)}:{Math.floor(currentTime - (Math.floor(currentTime / 60)) * 60)}
+            { `${formatTime(currentTime)}/${formatTime(duration)}` }
           </p>
-          <div className="player__timebar">
-            <div
-              className="player__timebar player__timebar-progress"
-              styles={{ width: (duration / 100) * currentTime }}
-            ></div>
-          </div>
+          <PlayerTimebar
+            duration={duration}
+            currentTime={currentTime}
+            onClick={time => {
+              myAudio.current.currentTime = time;
+            }}
+          />
         </div>
         <button className="player__button player__button_toggle">Релизы</button>
         <button className="player__button player__button_dropout"></button>
-        <audio className="audios" src={currentSong.audioFile} controls onTimeUpdate={onTimeUpdate}>
+        <audio
+          ref={myAudio}
+          className="audio"
+          src={currentSong.audioFile}
+          controls
+          onTimeUpdate={onTimeUpdate}
+          onLoadedData={() => {
+            setDuration(myAudio.current.duration)
+          }}
+          onPlay={onPlay}
+          onPause={onPause}
+        >
           audio not supported
         </audio>
       </div>
@@ -55,6 +95,9 @@ const Player = () => {
               item={item}
               onClick={(item) => {
                 setCurrentSong(item);
+                setCurrentTIme(0);
+                setDuration(myAudio.current.duration);
+                setIsPlaying(false);
               }}
             />
           ))}
