d9222cfbece386f752c885805698edbdcc0cf311
Никита Ковров <tochly@yandex.ru>
2020-11-11 08:06:32 +0300

    update: audio time countdown v0.1 :)

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,10 +1,22 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from 'react';
-import playlist from '../constants/playlist';
-import PlaylistItem from './PlaylistItem';
+import React, { useState } from "react";
+import throttle from "../utils/throttle";
+import playlist from "../constants/playlist";
+import PlaylistItem from "./PlaylistItem";
 
 const Player = () => {
-  const [ currentSong, setCurrentSong ] = useState(playlist[0])
+  const [currentSong, setCurrentSong] = useState(playlist[0]);
+  // currentTime setCurrentTime
+  const [currentTime, setCurrentTIme] = useState(0);
+  // duration setDuration
+  const [duration, setDuration] = useState(0);
+  // isPlaying
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const onTimeUpdate = throttle((e) => {
+    console.log(e.target.duration);
+    setCurrentTIme(e.target.duration - e.target.currentTime);
+  }, 1000);
 
   return (
     <div className="header__player-block player">
@@ -13,28 +25,39 @@ const Player = () => {
         <div className="player__song-container">
           <div className="player__current-song">
             <p className="player__song-title">
-            {currentSong.title} — {currentSong.author} <span style={{fontStyle: 'italic'}}>feat.</span> {currentSong.artist}
+              {currentSong.title} — {currentSong.author}{" "}
+              <span style={{ fontStyle: "italic" }}>feat.</span> {currentSong.artist}
             </p>
           </div>
-          <p className="player__song-duration">2:24</p>
+          <p className="player__song-duration">
+            {Math.floor(currentTime / 60)}:{Math.floor(currentTime - (Math.floor(currentTime / 60)) * 60)}
+          </p>
           <div className="player__timebar">
-            <div className="player__timebar player__timebar-progress"></div>
+            <div
+              className="player__timebar player__timebar-progress"
+              styles={{ width: (duration / 100) * currentTime }}
+            ></div>
           </div>
         </div>
         <button className="player__button player__button_toggle">Релизы</button>
         <button className="player__button player__button_dropout"></button>
+        <audio className="audios" src={currentSong.audioFile} controls onTimeUpdate={onTimeUpdate}>
+          audio not supported
+        </audio>
       </div>
       <div className="player__content-container">
         <h2 className="player__content-title">Релизы:</h2>
-        
+
         <ul className="player__playlist">
-          {playlist.map(item => <PlaylistItem
-            key={item.id}
-            item={item}
-            onClick={item => {
-              setCurrentSong(item)
-            }}
-          />)}
+          {playlist.map((item) => (
+            <PlaylistItem
+              key={item.id}
+              item={item}
+              onClick={(item) => {
+                setCurrentSong(item);
+              }}
+            />
+          ))}
         </ul>
       </div>
       {/* <div className="player__content-container">
@@ -45,6 +68,6 @@ const Player = () => {
               </div> */}
     </div>
   );
-}
+};
 
 export default Player;
---
+++ src/utils/throttle.js
@@ -0,0 +1,12 @@
+export default function throttle (callback, limit) {
+  let waiting = false;
+  return function () {
+    if (!waiting) {
+      callback.apply(this, arguments);
+      waiting = true;
+      setTimeout(function () {
+        waiting = false;
+      }, limit);
+    }
+  }
+}
