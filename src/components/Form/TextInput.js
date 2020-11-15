5b93e52817ab0a1896ea72c4a05bda0769e9cd8f
Yuliya Tsareva <tyv72@yandex.ru>
2020-11-16 00:52:14 +0700

    fix: fix form review comments

--- src/components/Form/TextInput.js
+++ src/components/Form/TextInput.js
@@ -11,11 +11,7 @@ const TextInput = ({ className, errorClassName, ...props }) => {
         className={meta.touched && meta.error ? `${className} ${className}_incorrect` : className} 
         {...props} 
       />
-      {meta.touched && meta.error ? (
-        <span className={errorClassName}>{meta.error}</span>
-      ) : (
-        <span className={`${errorClassName}_hidden`}></span>
-      )}
+      { meta.touched && meta.error && <span className={errorClassName}>{meta.error}</span> }
     </>
   );
 };
